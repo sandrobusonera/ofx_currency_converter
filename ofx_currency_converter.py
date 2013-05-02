57bcac96184ebccd40e0b6670db5d465580aaf30
Sandro <sandrobusonera@gmail.com>
2013-05-02 22:13:46 +0200

    convert date to ofx text

--- ofx_currency_converter.py
+++ ofx_currency_converter.py
@@ -19,10 +19,12 @@ def _get_template_context(ofx_file, exchange_rate):
     ofx = OfxParser.parse(file(ofx_file))
 
     context = dict(ofx.account.__dict__)
-    context['statement'] = dict(ofx.account.statement.__dict__)
+    context['statement'] = dict(context['statement'].__dict__)
     context['statement']['balance'] = _convert_amount(context['statement']['balance'], exchange_rate)
+    context['statement']['start_date'] = _date_to_text(context['statement']['start_date'])
+    context['statement']['end_date'] = _date_to_text(context['statement']['end_date'])
 
-    transactions = ofx.account.statement.transactions
+    transactions = context['statement']['transactions']
     context['statement']['transactions'] = _convert_amount_transactions(transactions, exchange_rate)
 
     return context
@@ -30,10 +32,14 @@ def _get_template_context(ofx_file, exchange_rate):
 def _convert_amount(amount, exchange_rate):
     return float(amount) * float(exchange_rate)
 
+def _date_to_text(date):
+    return "%s%s%s000000[-5:EST]" % (date.year, date.month, date.day)
+
 def _convert_amount_transactions(transactions, exchange_rate):
     transactions_converted = []
     for transaction in transactions:
         transaction.amount = _convert_amount(transaction.amount, exchange_rate)
+        transaction.date = _date_to_text(transaction.date)
         transactions_converted.append(transaction.__dict__)
 
     return transactions_converted
